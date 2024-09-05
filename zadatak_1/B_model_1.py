f3337d75c232317a496fe6e8608023c71d28237f
josip <josip.zivic93@gmail.com>
2024-09-06 00:42:27 +0200

    c1
    Signed-off-by: josip <josip.zivic93@gmail.com>

---
+++ zadatak_1/B_model_1.py
@@ -0,0 +1,36 @@
+from A_preprocessing import *
+
+
+
+
+
+class Model_1(object):
+
+    def __init__(self):
+        super(Model_1, self).__init__()
+
+        3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    DataOverview(csv_data=csv_input)
+
+    # all_data_df.to_pickle("all_data.pickle")
+
+
+
+
+
